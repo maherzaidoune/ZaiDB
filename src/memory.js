5bc818efe09a853011386f5279e62c2d7d270c5f
Maher Zaydoun <maher.zaydoun@satoripop.com>
2020-05-12 02:18:15 +0100

    add pop method

--- src/memory.js
+++ src/memory.js
@@ -1,4 +1,5 @@
 var cache = {}
+var size = 0;
 
 const get = index => typeof cache[index] !== 'undefined' ? cache[index] : null;
 const set = (index, value) => {
@@ -6,4 +7,21 @@ const set = (index, value) => {
         cache[index] = [];
     }
     cache[index].push(value);
+    size++;
+}
+const size = () => {
+    return size;
+}
+const pop = (index, value) => {
+    if (index > size) {
+        pop(size, value);
+    }
+    if (typeof cache[index] === 'undefined') {
+        return null;
+    }
+    const valueAtIndex = cache[index].indexOf(value);
+    if(valueAtIndex != -1){
+        cache[index].splice(valueAtIndex, 1);
+    }
+    return valueAtIndex;
 }
